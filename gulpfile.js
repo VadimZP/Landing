c52fbd303d6a1e400492109280aa5f22a19b1d98
vadim <wesborlandzp@gmail.com>
2017-10-30 14:43:24 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
+const autoprefixer = require("gulp-autoprefixer");
+const plumber = require('gulp-plumber');
+
+gulp.task("sass", function () {
+    return gulp.src("./sass/**/*.scss")
+        .pipe(plumber())
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .on('error', catchErr)
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+function catchErr(e) {
+    console.log(e);
+    this.emit('end');
+}
+
+gulp.task("browser-sync", function () {
+    browserSync({
+        server: {
+            baseDir: "./",
+        },
+        notify: false
+    })
+});
+
+gulp.task("watch", ["browser-sync", "sass"], function () {
+    gulp.watch("./*.html", browserSync.reload);
+    gulp.watch("./sass/**/*.scss", ["sass"]);
+});
